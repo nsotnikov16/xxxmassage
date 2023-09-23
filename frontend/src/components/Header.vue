da28dfb16b04fca0dadf2df1b05a1f335353df3e
Nikita Sotnikov <nsotnikov16@yandex.ru>
2023-09-23 13:46:04 +0500

    update

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -8,7 +8,7 @@ import HeaderNavigation from './HeaderNavigation.vue';
   <header class="p-3 mb-3 border-bottom">
     <div class="container">
       <div
-        class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-between"
+        class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-between flex-column flex-md-row"
       >
         <router-link :to="{name: 'home'}"
           class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
