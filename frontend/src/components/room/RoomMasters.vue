addfcd790417f5f9bdd9c9642ba308b34af4c124
Nikita Sotnikov <nsotnikov16@yandex.ru>
2023-09-24 10:35:39 +0500

    update

--- frontend/src/components/room/RoomMasters.vue
+++ frontend/src/components/room/RoomMasters.vue
@@ -4,85 +4,95 @@ import Multiselect from "vue-multiselect";
 </script>
 
 <template>
-  <div class="room__block">
-    <div class="room__block-name">
-      <Person />
-      <b>Мастер:</b>
+    <div class="room__block">
+        <div class="room__block-name">
+            <Person />
+            <b>Мастер:</b>
+        </div>
+        <div class="room__select">
+            <multiselect
+                v-model="value"
+                :multiple="true"
+                deselect-label="Удалить"
+                track-by="name"
+                label="name"
+                placeholder="Выберите мастера"
+                :options="options"
+                :searchable="true"
+                selectLabel=""
+                selectedLabel="Выбрано"
+                :close-on-select="false"
+                @select="changeMaster"
+                @remove="(val) => changeMaster(val, 'delete')"
+                :disabled="disabled"
+            >
+                <template #noResult> Поиск не дал результатов </template>
+            </multiselect>
+        </div>
     </div>
-    <div class="room__select">
-      <multiselect
-        v-model="value"
-        :multiple="true"
-        deselect-label="Удалить"
-        track-by="name"
-        label="name"
-        placeholder="Выберите мастера"
-        :options="options"
-        :searchable="true"
-        selectLabel=""
-        selectedLabel="Выбрано"
-        :close-on-select="false"
-        @select="changeMaster"
-        @remove="(val) => changeMaster(val, 'delete')"
-        :disabled="disabled"
-      >
-        <template #noResult> Поиск не дал результатов </template>
-      </multiselect>
-    </div>
-  </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
 export default {
-  name: "RoomMasters",
-  props: ["salonId", "roomId"],
-  data() {
-    return {
-      value: [],
-      disabled: false,
-    };
-  },
-  computed: {
-    ...mapGetters(["getEntityBySalonId"]),
-    options() {
-      return this.getEntityBySalonId(this.salonId, "masters");
+    name: "RoomMasters",
+    props: ["salonId", "roomId"],
+    data() {
+        return {
+            value: [],
+            disabled: false,
+        };
     },
-  },
-  methods: {
-    ...mapActions(["updateRoomForMaster", "setModalSettings"]),
-    updateValue() {
-      try {
-        this.value = this.options.filter((m) => m.room_id == this.roomId);
-      } catch (error) {
-        this.value = [];
-      }
+    computed: {
+        ...mapGetters(["getEntityBySalonId"]),
+        options() {
+            const options = this.getEntityBySalonId(this.salonId, "masters");
+            this.updateValue(options);
+            return options;
+        },
     },
-    async changeMaster(master, type) {
-      if (!type) type = "add";
-      this.disabled = true;
-      const result = await this.updateRoomForMaster({
-        masterId: master.id,
-        roomId: this.roomId,
-        type,
-      });
+    watch: {
+        options() {
+            this.updateValue();
+        },
+    },
+    methods: {
+        ...mapActions(["updateRoomForMaster", "setModalSettings"]),
+        updateValue() {
+            try {
+                this.value = this.options.filter(
+                    (m) => m.room_id == this.roomId
+                );
+            } catch (error) {
+                this.value = [];
+            }
+        },
+        async changeMaster(master, type) {
+            if (!type) type = "add";
+            this.disabled = true;
+            const result = await this.updateRoomForMaster({
+                masterId: master.id,
+                roomId: this.roomId,
+                type,
+            });
 
-      if (result?.error) {
-        this.setModalSettings({
-          show: true,
-          title: result.error,
-          fields: [],
-          btnSave: false,
-          btnClose: { title: "Закрыть" },
-        });
-      }
-      this.updateValue();
-      this.disabled = false;
+            if (result?.error) {
+                this.setModalSettings({
+                    show: true,
+                    title: result.error,
+                    fields: [],
+                    btnSave: false,
+                    btnClose: { title: "Закрыть" },
+                });
+                this.updateValue();
+            }
+
+            this.disabled = false;
+        },
     },
-  },
-  created() {
-    this.updateValue();
-  },
+    created() {
+        this.updateValue();
+    },
 };
 </script>
 
--- frontend/src/components/room/RoomProgramms.vue
+++ frontend/src/components/room/RoomProgramms.vue
@@ -4,87 +4,89 @@ import Multiselect from "vue-multiselect";
 </script>
 
 <template>
-  <div class="room__block">
-    <div class="room__block-name">
-      <Programm />
-      <b>Программа:</b>
+    <div class="room__block">
+        <div class="room__block-name">
+            <Programm />
+            <b>Программа:</b>
+        </div>
+        <div class="room__select">
+            <multiselect
+                v-model="value"
+                :multiple="false"
+                deselect-label="Удалить"
+                track-by="name"
+                label="name"
+                placeholder="Выберите программу"
+                :options="options"
+                :searchable="true"
+                selectLabel=""
+                selectedLabel="Выбрано"
+                :show-labels="true"
+                @select="changeProgramm"
+                @remove="(val) => changeProgramm(val, 'delete')"
+            >
+                <template #noResult> Поиск не дал результатов </template>
+            </multiselect>
+        </div>
     </div>
-    <div class="room__select">
-      <multiselect
-        v-model="value"
-        :multiple="false"
-        deselect-label="Удалить"
-        track-by="name"
-        label="name"
-        placeholder="Выберите программу"
-        :options="options"
-        :searchable="true"
-        selectLabel=""
-        selectedLabel="Выбрано"
-        :show-labels="true"
-        @select="changeProgramm"
-        @remove="(val) => changeProgramm(val, 'delete')"
-      >
-        <template #noResult> Поиск не дал результатов </template>
-      </multiselect>
-    </div>
-  </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 export default {
-  name: "RoomProgramms",
-  props: ["salonId", "room"],
-  data() {
-    return {
-      value: {},
-    };
-  },
-  computed: {
-    ...mapGetters(["getEntityBySalonId"]),
-    options() {
-      return this.getEntityBySalonId(this.salonId, "programms");
+    name: "RoomProgramms",
+    props: ["salonId", "room"],
+    data() {
+        return {
+            value: {},
+        };
     },
-  },
-  methods: {
-    ...mapActions(["updateProgrammInRoom", "setModalSettings"]),
-    updateValue() {
-      try {
-        this.value = this.options.filter((m) => m.id == this.room.programm_id);
-      } catch (error) {
-        this.value = [];
-      }
+    watch: {
+        options() {
+            setTimeout(this.updateValue, 100);
+        },
     },
-    async changeProgramm(val, type) {
-      this.disabled = true;
-      const result = await this.updateProgrammInRoom({
-        roomId: this.room.id,
-        programm: type == "delete" ? 0 : val.id,
-      });
-
-      if (result?.error) {
-        this.setModalSettings({
-          show: true,
-          title: result.error,
-          fields: [],
-          btnSave: false,
-          btnClose: { title: "Закрыть" },
-        });
-
-        if (type == "delete") {
-          this.value = this.value.push(val);
-        } else {
-          this.value = {};
-        }
-      }
-
-      this.disabled = false;
+    computed: {
+        ...mapGetters(["getEntityBySalonId"]),
+        options() {
+            return this.getEntityBySalonId(this.salonId, "programms");
+        },
     },
-  },
-  created() {
-    this.updateValue();
-  },
+    methods: {
+        ...mapActions(["updateProgrammInRoom", "setModalSettings"]),
+        updateValue() {
+            try {
+                this.value = this.options.filter(
+                    (m) => m.id == this.room.programm_id
+                );
+            } catch (error) {
+                this.value = [];
+            }
+        },
+        async changeProgramm(val, type) {
+            this.disabled = true;
+            const result = await this.updateProgrammInRoom({
+                roomId: this.room.id,
+                programm: type == "delete" ? 0 : val.id,
+            });
+
+            if (result?.error) {
+                this.setModalSettings({
+                    show: true,
+                    title: result.error,
+                    fields: [],
+                    btnSave: false,
+                    btnClose: { title: "Закрыть" },
+                });
+                this.updateValue();
+            }
+
+            this.disabled = false;
+        },
+    },
+    created() {
+        this.updateValue();
+    },
 };
 </script>
 
